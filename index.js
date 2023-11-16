7b50cb728fb21f81746d4e3f8cb07134ce945f6d
Marline John <marlinejohn@gmail.com>
2023-11-16 23:34:24 +0100

    renamed image file

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ let travelList =[
         second: "Boat/ cruise tour",
         third: "Coffeeshops"
     },
-    img: "./images/amsterdam.jpg",
+    img: "./images/amsterdam1.jpg",
    },
 {
     id:"baku",
